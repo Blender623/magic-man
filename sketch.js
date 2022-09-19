df48d55b863c38c10c023f05d5dc18bc408d4fec
Blender623 <107869607+Blender623@users.noreply.github.com>
2022-09-19 09:57:57 +0530

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let song,amp,level
+function preload() {
+  song =loadSound('https://cdn.pixabay.com/audio/2022/08/20/audio_55aa207a92.mp3');
+}
+function setup(){
+  createCanvas(400,400)
+  song.play()
+  amp = new p5.Amplitude()
+}
+function draw(){
+  background(255)
+ level= amp.getLevel()
+  value= map(level,0,1,0,360);
+  translate(width/2,height/2);
+  strokeWeight(10)
+  arc(0,0,200,200,0,value/30)
+  rotate(PI)
+  line(100,100,value,value)
+   arc(0,0,100,100,0,value/30)
+  rotate(PI/2)
+  line(100,100,value,value)
+  
+}
