99f6da4d817ad5bb467399a6adc51cf9644e392e
Blender623 <107869607+Blender623@users.noreply.github.com>
2023-03-27 20:40:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function preload() {
  //song= loadSound('https://assets.editor.p5js.org/62316d91ad2f6600243abedc/eb36f330-0bc7-4a84-9315-8a792028fced.mp3');
 }
 function setup(){
-  createCanvas(window.inner.width,window.inner.height)
+  createCanvas(window.innerWidth,window.innerHeight)
  
   amp = new p5.Amplitude()
    getAudioContext().resume();
@@ -23,23 +23,20 @@ function mousePressed() {
 function draw(){
   background(255)
  level= amp.getLevel()
-   if(value>200){
-    fill(value,0,0);
-  }if(value>300){
-    fill(255,255,0)
-  }
-  if(value>240){
-    fill(0,0,234)
-  }
+ 
   value= map(level,0,1,0,360);
+ 
   translate(width/2,height/2);
   strokeWeight(10)
   arc(0,0,window.width/2,window.width/2,0,value/30)
   rotate(PI)
-   stroke(255,0,value,value)
+   stroke(255,0,0,value);
+
   //fill(level);
   line(100,100,value,value)
-   arc(0,0,100,100,0,value/30)
+   arc(value+60,value+60,150,150,0,value/30)
+     
+     arc(0,0,100,100,0,value/15)
   rotate(PI/2)
   line(100,100,value,value)
   
