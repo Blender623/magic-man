d3ca8b65ed19849b11ac747865e934f495e322a0
Blender623 <107869607+Blender623@users.noreply.github.com>
2023-03-27 20:08:48 +0530

    new song & colour
    added some colours and new song

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let song,amp,level
 function preload() {
  // song =loadSound('https://cdn.pixabay.com/audio/2022/08/20/audio_55aa207a92.mp3');
- song= loadSound('https://cdn.pixabay.com/download/audio/2023/01/28/audio_a153587dd5.mp3');//filename=phonk-136933.mp3
+// song= loadSound('https://cdn.pixabay.com/download/audio/2023/01/28/audio_a153587dd5.mp3');//filename=phonk-136933.mp3
+ song= loadSound('https://assets.editor.p5js.org/62316d91ad2f6600243abedc/eb36f330-0bc7-4a84-9315-8a792028fced.mp3');
 }
 function setup(){
   createCanvas(window.screen.width,window.screen.height)
@@ -22,11 +23,20 @@ function mousePressed() {
 function draw(){
   background(255)
  level= amp.getLevel()
+   if(value>200){
+    fill(value,0,0);
+  }if(value>300){
+    fill(255,255,0)
+  }
+  if(value>240){
+    fill(0,0,234)
+  }
   value= map(level,0,1,0,360);
   translate(width/2,height/2);
   strokeWeight(10)
   arc(0,0,window.width/2,window.width/2,0,value/30)
   rotate(PI)
+   stroke(255,0,value,value)
   //fill(level);
   line(100,100,value,value)
    arc(0,0,100,100,0,value/30)
